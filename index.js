2a5399c0f4ff5fad74ab4144ac4e17dfaedc3f1a
holanda-h2 <43278200+holanda-h2@users.noreply.github.com>
2022-12-17 12:58:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/game.html')
+})
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
